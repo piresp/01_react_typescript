985351817f529e35c557ed1e323790943413bbf4
piresp <gabrielfpp47@gmail.com>
2022-10-26 17:00:44 -0300

    aula_01_criando_botao
    botaozinho

---
+++ studies/src/components/botao/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Botao extends React.Component {
+    render() {
+        return (
+            <button>
+                Botãozinho
+            </button>
+        )
+    }
+}
+
+export default Botao;
