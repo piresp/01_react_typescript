ee2dc010e34a5c37ea2519e08299f8aebe57f543
piresp <gabrielfpp47@gmail.com>
2022-10-27 11:36:40 -0300

    aula_03_css_sass
    aplicando estilizacao com sass

--- studies/src/components/formulario/index.tsx
+++ studies/src/components/formulario/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Botao from '../botao';
+import './style.scss';
 
 class Formulario extends React.Component {
     render() {
         return (
-            <form>
-                <div>
+            <form className="novaTarefa">
+                <div className="inputContainer">
                     <label htmlFor="tarefa">Adicione um estudo</label>
                     <input name="tarefa" id="tarefa" placeholder="O que você quer estudar" type="text"/>
                 </div>
-                <div>
+                <div className="inputContainer">
                     <label htmlFor="tempo">Tempo</label>
                     <input name="tempo" id="tempo" step="1" min="00:00:00" max="01:30:00" type="time" required/>
                 </div>
---
+++ studies/src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Formulario from '../components/formulario';
+import Lista from '../components/lista';
+import './style.scss';
+
+function App() {
+  return (
+    <div className="AppStyle">
+        <Formulario />
+        <Lista />
+    </div>
+  );
+}
+
+export default App;
