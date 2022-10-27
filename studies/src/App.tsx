f79beeb28746702dba600f1237319f26509a83bb
piresp <gabrielfpp47@gmail.com>
2022-10-27 10:25:43 -0300

    aula_02_lista_dinamica
    criando lista com reendenizacao dinamica, componente criado por funcao

--- studies/src/App.tsx
+++ studies/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Formulario from './components/formulario';
+import Lista from './components/lista';
 
 function App() {
   return (
     <div className="App">
         <Formulario />
+        <Lista />
     </div>
   );
 }
