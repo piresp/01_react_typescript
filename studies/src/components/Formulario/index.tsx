d663a9e9f2891ea7c693c80d1a21deaa86c019a0
piresp <gabrielfpp47@gmail.com>
2022-11-01 16:37:12 -0300

    aula_05_state_formulario
    pegando objetos por meio de eventos dentro do Formulario

--- studies/src/components/Formulario/index.tsx
+++ studies/src/components/Formulario/index.tsx
@@ -3,16 +3,28 @@ import Botao from '../Botao';
 import style from './Formulario.module.scss';
 
 class Formulario extends React.Component {
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        console.log('state: ', this.state);
+    }
+
     render() {
         return (
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">Adicione um estudo</label>
-                    <input name="tarefa" id="tarefa" placeholder="O que você quer estudar" type="text"/>
+                    <input name="tarefa" id="tarefa" placeholder="O que você quer estudar" type="text"
+                        value={this.state.tarefa} onChange={ evento => this.setState({ ...this.state, tarefa: evento.target.value })}/>
                 </div>
                 <div className={style.inputContainer}>
                     <label htmlFor="tempo">Tempo</label>
-                    <input name="tempo" id="tempo" step="1" min="00:00:00" max="01:30:00" type="time" required/>
+                    <input name="tempo" id="tempo" step="1" min="00:00:00" max="01:30:00" type="time" 
+                        value={this.state.tempo} onChange={ evento => this.setState({ ...this.state, tempo: evento.target.value })} required/>
                 </div>
                 <Botao>
                     Botaozinho
