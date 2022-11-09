086cf4ac24a7c1dcd84646bd8cb3faba88b32f45
piresp <gabrielfpp47@gmail.com>
2022-11-09 16:11:02 -0300

    aula_06_selecionando_item_scss
    Separando scss de Item

--- studies/src/components/Lista/Item/index.tsx
+++ studies/src/components/Lista/Item/index.tsx
@@ -1,5 +1,5 @@
-import { Itarefa } from '../../../types/tarefa'
-import style from '../Lista.module.scss'
+import { Itarefa } from '../../../types/tarefa';
+import style from './Item.module.scss';
 
 interface Props extends Itarefa {
     selecionaTarefa: (tarefaSelecionada: Itarefa) => void
@@ -13,7 +13,6 @@ export default function Item(
         id,
         selecionaTarefa
     } : Props) {
-    console.log('Item atual: ', { tarefa, tempo, selecionado, completado, id })
     return (
         <li 
             className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`} 
